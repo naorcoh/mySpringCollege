0a28d9c15e79fc1f67ec0431ceb99fe46bf50780
Naorcoh <naorcohen92@gmail.com>
2022-09-18 00:49:53 +0300

    request scoop example

--- src/main/java/com/example/SpringBootCollegeApp/sevice/ContactService.java
+++ src/main/java/com/example/SpringBootCollegeApp/sevice/ContactService.java
@@ -1,15 +1,21 @@
 package com.example.SpringBootCollegeApp.sevice;
 
 import com.example.SpringBootCollegeApp.model.Contact;
+import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.context.annotation.RequestScope;
+import org.springframework.web.context.annotation.SessionScope;
 
 @Service
+@Data
+@RequestScope
 @Slf4j
 public class ContactService {
+    private int counter = 0;
 
     /**
      *
@@ -18,6 +24,9 @@ public class ContactService {
      */
 
     public boolean saveMessageDetails(Contact contact) {
+
+        log.info("Contact Service bean was initialized!");
+
         log.info(contact.toString());
         return true;
 

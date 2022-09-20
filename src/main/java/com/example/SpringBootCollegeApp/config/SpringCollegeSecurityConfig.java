a9e542573cd57fb620ae9660adb2929ccb1f39ef
Naorcoh <naorcohen92@gmail.com>
2022-09-20 22:04:27 +0300

    fix SpringCollegeSecurityConfig

--- src/main/java/com/example/SpringBootCollegeApp/config/SpringCollegeSecurityConfig.java
+++ src/main/java/com/example/SpringBootCollegeApp/config/SpringCollegeSecurityConfig.java
@@ -3,10 +3,18 @@ package com.example.SpringBootCollegeApp.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SpringCollegeSecurityConfig {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
@@ -21,5 +29,10 @@ public class SpringCollegeSecurityConfig {
                 .and().httpBasic();
 
         return http.build();
+    }
+
+    @Bean
+    public InMemoryUserDetailsManager userDetailsService() {
+        return new InMemoryUserDetailsManager();
     }
 }

344f225e0394f9f026281e822e180810e87f3a27
Naorcoh <naorcohen92@gmail.com>
2022-09-25 21:02:53 +0300

    create repository package and create new ContactRepository class with @Repository stereotype annotations

--- src/main/java/com/example/SpringBootCollegeApp/sevice/ContactService.java
+++ src/main/java/com/example/SpringBootCollegeApp/sevice/ContactService.java
@@ -1,13 +1,17 @@
 package com.example.SpringBootCollegeApp.sevice;
 
 import com.example.SpringBootCollegeApp.model.Contact;
+import com.example.SpringBootCollegeApp.repository.ContactRepository;
 import lombok.extern.slf4j.Slf4j;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 @Slf4j
 public class ContactService {
+    @Autowired
+    ContactRepository contactRepository;
 
     /**
      *

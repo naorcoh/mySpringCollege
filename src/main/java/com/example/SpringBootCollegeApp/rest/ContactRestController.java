800d7a9ea75a6afcff55c1a95d9fe84b0bbc6321
Naorcoh <naorcohen92@gmail.com>
2022-10-12 16:54:48 +0300

    -in ContactRestController delete @ResponseBody from the method and change @Controller to @RestController that return all the data from the method as json object

--- src/main/java/com/example/SpringBootCollegeApp/rest/ContactRestController.java
+++ src/main/java/com/example/SpringBootCollegeApp/rest/ContactRestController.java
@@ -1,7 +1,6 @@
 package com.example.SpringBootCollegeApp.rest;
 
 import com.example.SpringBootCollegeApp.model.Contact;
-import com.example.SpringBootCollegeApp.model.enums.EInquiryStatus;
 import com.example.SpringBootCollegeApp.repository.ContactRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -10,20 +9,18 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Collections;
 import java.util.List;
 
-@Controller
+@RestController
 @RequestMapping(path = "/api/inquiries")
 public class ContactRestController {
     @Autowired
     private ContactRepository contactRepository;
 
     @GetMapping("/getInquiriesByStatus")
-    @ResponseBody
     public List<Contact> getInquiriesByStatus(@RequestParam String status) {
         return contactRepository.findByStatus(status);
     }
 
     @GetMapping("/getInquiriesBySubject")
-    @ResponseBody
     public List<Contact> getInquiriesBySubject(@RequestBody Contact contact) {
         if (contact != null && contact.getSubject() != null)
             return contactRepository.findBySubjectContaining(contact.getSubject());
